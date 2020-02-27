71a6976d226ba26962da64322d37e5a6b525b0fa
Renato Yoiti <renatoyoiti@gmail.com>
2020-02-27 10:09:04 -0300

    feat: Add Cart page style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,69 @@
 import React from 'react';
 
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
+
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProductTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QNT</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <img
+                src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
+                alt="Tênis"
+              />
+            </td>
+            <td>
+              <strong>Tênis Massa</strong>
+              <span>R$399,90</span>
+            </td>
+            <td>
+              <div>
+                <button type="button">
+                  <MdRemoveCircleOutline size={20} color="#7159c1" />
+                </button>
+                <input type="number" readOnly value={2} />
+                <button type="button">
+                  <MdAddCircleOutline size={20} color="#7159c1" />
+                </button>
+              </div>
+            </td>
+            <td>
+              <strong>R$799,80</strong>
+            </td>
+            <td>
+              <button type="button">
+                <MdDelete size={20} color="#7159c1" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </ProductTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+
+        <Total>
+          <span>TOTAL</span>
+          <strong>R$799,80</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
