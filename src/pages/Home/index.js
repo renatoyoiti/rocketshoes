0decc9e07f04bf7fe1785ce715b2d2925ab364ce
Renato Yoiti <renatoyoiti@gmail.com>
2020-02-27 10:25:52 -0300

    feat: Add number format utils and API request to Main page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,72 +1,50 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
+
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 
 import { ProductList } from './styles';
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Adidas</strong>
-        <span>R$399,90</span>
+export default class Home extends Component {
+  constructor() {
+    super();
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Adidas</strong>
-        <span>R$399,90</span>
+    this.state = {
+      products: [],
+    };
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Adidas</strong>
-        <span>R$399,90</span>
+  async componentDidMount() {
+    const response = await api.get('products');
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
-          alt="Tênis"
-        />
-        <strong>Tênis Adidas</strong>
-        <span>R$399,90</span>
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
