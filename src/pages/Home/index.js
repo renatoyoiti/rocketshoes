17c58637b31ca32bdca7dc587ae391eb2159cfce
Renato Yoiti <renatoyoiti@gmail.com>
2020-02-27 09:40:33 -0300

    feat: Add Home page style

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,72 @@
 import React from 'react';
 
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas</strong>
+        <span>R$399,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas</strong>
+        <span>R$399,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas</strong>
+        <span>R$399,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=544xhttps://static.netshoes.com.br/produtos/tenis-adidas-fitbounce-masculino/06/NQQ-2765-006/NQQ-2765-006_zoom3.jpg??ims=1088x"
+          alt="Tênis"
+        />
+        <strong>Tênis Adidas</strong>
+        <span>R$399,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
