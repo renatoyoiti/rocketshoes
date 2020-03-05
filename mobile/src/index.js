46246656b125cf5beeb9e0faa03db1b7f5b82233
Renato Yoiti <renatoyoiti@gmail.com>
2020-03-04 22:12:06 -0300

    mobile: Initial Commit

---
+++ mobile/src/index.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import { StatusBar, View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#191920" />
+    </>
+  );
+};
+
+export default App;
